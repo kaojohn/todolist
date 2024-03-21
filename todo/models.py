54c4d58cc293317bd3458237c76c57ed0002c91f
johnkao <liberbay105@gmail.com>
2024-03-21 21:39:01 +0900

    todolist後端及基本頁面

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+
+    # user_id
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # 更改顯示效果
+    def __str__(self):
+        return f"[{self.created}] {self.title}-({self.user})"
