6ac960a52aeb2de2d581f9ae75d6699c41a40763
johnkao <liberbay105@gmail.com>
2024-03-26 20:44:48 +0900

    建立事項表單

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoForm
 
 
 # Create your views here.
-def todo(request):
+
+
+def create_todo(request):
+    # GET
+    message = ""
+    form = TodoForm()
+    # POST
+    if request.method == "POST":
+        try:
+            # print(request.POST)
+            form = TodoForm(request.POST)
+            new_todo = form.save(commit=False)
+            new_todo.user = request.user
+            new_todo.save()
+            message = "建立成功"
+
+            return redirect("todolist")
+
+        except Exception as e:
+            print(e)
+            message = "建立失敗"
+
+    return render(request, "todo/create-todo.html", {"form": form, "message": message})
+
+
+def todolist(request):
     # all(所有),get,filter
     # todos = Todo.objects.all()
     todos = None
