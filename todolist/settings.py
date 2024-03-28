997b4c5d6f0ba07d2288626137d12a46efdd75a3
johnkao <liberbay105@gmail.com>
2024-03-28 21:21:21 +0900

    完成mysql連結

--- todolist/settings.py
+++ todolist/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +78,23 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# cql 本地端
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# mysql
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "todolist",
+        "USER": "root",
+        "PASSWORD": "123456789",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
